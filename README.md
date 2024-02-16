1bb60c922cba8cd0a3128fb223fd8ef57ac76af5
nerfZael <bogunovij@gmail.com>
2024-02-16 23:28:17 +0100

    multisend implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sage-agent
+
+To deploy safe to local network:
+- Clone: https://github.com/safe-global/safe-smart-account/ (branch: `v1.3.0`)
+- Run: `yarn && yarn hardhat node`
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,7 @@ from .get_eth_balance import get_eth_balance
 from .send_eth import send_eth
 from .deploy_safe_with_create2 import deploy_safe_with_create2
 from .generate_agent_account import generate_agent_account
-from .constants import MASTER_COPY_ADDRESS, PROXY_FACTORY_ADDRESS
+from .constants import MASTER_COPY_ADDRESS, PROXY_FACTORY_ADDRESS, MULTI_SEND_ADDRESS
 from .deploy_multicall import deploy_multicall
 from .send_tx import send_tx
 from .cache import cache
@@ -16,6 +16,7 @@ __all__ = [
     "generate_agent_account",
     "MASTER_COPY_ADDRESS",
     "PROXY_FACTORY_ADDRESS",
+    "MULTI_SEND_ADDRESS",
     "deploy_multicall",
     "send_tx",
     "cache"
