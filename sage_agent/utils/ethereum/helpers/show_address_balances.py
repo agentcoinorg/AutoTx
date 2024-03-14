0758be9645a7926963586eb976ba83dc1920ebcf
Cesar <cesarbrazon10@gmail.com>
2024-03-14 21:52:24 +0100

    chore: undo change in show address balances

--- sage_agent/utils/ethereum/helpers/show_address_balances.py
+++ sage_agent/utils/ethereum/helpers/show_address_balances.py
@@ -11,24 +11,22 @@ weth_address = contracts_config["erc20"]["weth"]
 usdc_address = contracts_config["erc20"]["usdc"]
 wbtc_address = contracts_config["erc20"]["wbtc"]
 
-def show_address_balances(web3: Web3, address: str, verbose: Optional[bool]):
+
+def show_address_balances(web3: Web3, address: str):
     dai_balance = get_erc20_balance(web3, dai_address, address)
     usdc_balance = get_erc20_balance(web3, usdc_address, address)
     wbtc_balance = get_erc20_balance(web3, wbtc_address, address)
     eth_balance = web3.eth.get_balance(address)
     weth_balance = get_erc20_balance(web3, weth_address, address)
 
-    if verbose:
-        print(
-            dedent(
-                f"""
-                DAI Balance: {dai_balance / 10 ** 18}
-                USDC Balance: {usdc_balance / 10 ** 6}
-                WBTC Balance: {wbtc_balance / 10 ** 8}
-                ETH Balance: {eth_balance / 10 ** 18}
-                WETH Balance: {weth_balance / 10 ** 18}
-                """
-            )
+    print(
+        dedent(
+            f"""
+            DAI Balance: {dai_balance / 10 ** 18}
+            USDC Balance: {usdc_balance / 10 ** 6}
+            WBTC Balance: {wbtc_balance / 10 ** 8}
+            ETH Balance: {eth_balance / 10 ** 18}
+            WETH Balance: {weth_balance / 10 ** 18}
+            """
         )
-
-    
+    )
