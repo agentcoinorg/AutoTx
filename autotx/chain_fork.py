9ae933473a4d5f009d4b600616690ea66d75f112
Cesar <cesarbrazon10@gmail.com>
2024-03-25 21:54:29 +0100

    chore: better messages in errors related to local node

--- autotx/chain_fork.py
+++ autotx/chain_fork.py
@@ -1,11 +1,22 @@
 import subprocess
+import sys
 from autotx.utils.ethereum.cached_safe_address import delete_cached_safe_address
 
 container_name = "autotx_chain_fork"
 
+
 def start():
     delete_cached_safe_address()
-    subprocess.run(["docker", "build", "-t", "autotx_chain_fork", "."], check=True)
+
+    build = subprocess.run(
+        ["docker", "build", "-t", "autotx_chain_fork", "."], capture_output=True
+    )
+
+    if build.returncode != 0:
+        sys.exit(
+            "Local node start up has failed. Make sure you have docker desktop installed and running"
+        )
+
     subprocess.run(
         [
             "docker",
@@ -21,6 +32,7 @@ def start():
         ],
         check=True,
     )
+
 
 def stop():
     subprocess.run(["docker", "container", "rm", container_name, "-f"], check=True)
