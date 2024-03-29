3380ccbba58c0644a6b3de90f0019d43cd19d4e6
nerfZael <bogunovij@gmail.com>
2024-03-29 13:36:26 +0100

    comment fixes

--- autotx/tests/token/test_swap.py
+++ autotx/tests/token/test_swap.py
@@ -27,11 +27,11 @@ def test_auto_tx_swap_multiple(configuration, auto_tx):
     usdc_address = ETHAddress(network_info.tokens["usdc"], web3)
     wbtc_address = ETHAddress(network_info.tokens["wbtc"], web3)
 
-    prompt = "Buy 1000 USDC with ETH and then buy 0.05 WBTC with the USDC"
+    prompt = "Buy 1000 USDC with ETH and then buy 0.01 WBTC with the USDC"
     usdc_balance = manager.balance_of(usdc_address)
     wbtc_balance = manager.balance_of(wbtc_address)
 
     auto_tx.run(prompt, non_interactive=True)
 
     assert usdc_balance + 1000 == manager.balance_of(usdc_address)
-    assert wbtc_balance + 0.05 == manager.balance_of(wbtc_address)
+    assert wbtc_balance + 0.01 == manager.balance_of(wbtc_address)
