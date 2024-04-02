79962d43e43327fdb29946a82b5db2ecf2668dbd
Zhou <1359698378@qq.com>
2024-04-02 21:42:24 +0800

    Add support for local LLM

--- autotx/utils/agent/build_goal.py
+++ autotx/utils/agent/build_goal.py
@@ -1,4 +1,5 @@
 import json
+import os
 from textwrap import dedent
 import typing
 
@@ -12,7 +13,7 @@ class GoalResponse:
 
     def __init__(self, goal: str):
         self.goal = goal
-    
+
 class MissingInfoResponse:
     message: str
     type: str = "missing_info"
@@ -103,7 +104,7 @@ def analyze_user_prompt(chat_history: str, agents_information: str, smart_accoun
     )
 
     response = openai.chat.completions.create(
-        model="gpt-4-turbo-preview",
+        model=os.environ.get("OPENAI_MODEL_NAME", "gpt-4-turbo-preview"),
         response_format={"type": "json_object"},
         messages=[
             { "role": "system", "content": get_persona(smart_account_address) },
