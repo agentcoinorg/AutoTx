6a3f563fe6fe74748cd6e693a464027efc00c336
Cesar <cesarbrazon10@gmail.com>
2024-04-09 23:24:56 +0200

    chore: create build script in gh action

---
+++ autotx/build_check.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+def run() -> None:
+    # Run mypy check
+    result = subprocess.run(["mypy", "."], capture_output=True)
+    print(result.stdout.decode())
+    if result.returncode != 0:
+        print("Type checking failed")
+        sys.exit(1)
