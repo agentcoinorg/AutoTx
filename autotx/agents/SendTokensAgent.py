d53bc8caf6edba675e0005678080082c4e372e57
nerfZael <bogunovij@gmail.com>
2024-04-10 18:32:08 +0200

    unified erc20 and native token functions

--- autotx/agents/SendTokensAgent.py
+++ autotx/agents/SendTokensAgent.py
@@ -11,54 +11,26 @@ from autotx.utils.ethereum import (
 from autotx.utils.ethereum import load_w3
 from autotx.utils.ethereum.build_transfer_eth import build_transfer_eth
 from web3.constants import ADDRESS_ZERO
+from autotx.utils.ethereum.constants import NATIVE_TOKEN_ADDRESS
 from autotx.utils.ethereum.eth_address import ETHAddress
+from autotx.utils.ethereum.get_eth_balance import get_eth_balance
 
 name = "send-tokens"
 
 system_message = lambda autotx: dedent(f"""
     You are an AI assistant. Assist the user (address: {autotx.manager.address}) in their tasks by fetching balances and preparing transactions to send tokens.
-    You are an expert in Ethereum tokens and can help users send tokens and check their balances.
+    You are an expert in Ethereum tokens (native and erc20) and can help users send tokens and check their balances.
     You use the tools available to assist the user in their tasks. 
     Your job is to only prepare the transactions and the user will take care of executing them.
-    NOTE: There is no reason to call get_erc20_balance after calling transfer as the transfers are only prepared and not executed. 
+    NOTE: There is no reason to call get_balance after calling transfer as the transfers are only prepared and not executed. 
     """
 )
 
-class TransferETHTool(AutoTxTool):
-    name: str = "prepare_transfer_eth_transaction"
+class TransferTokenTool(AutoTxTool):
+    name: str = "prepare_transfer_transaction"
     description: str = dedent(
         """
-        Prepares a transfer transaction for given amount in decimals for ETH.
-        """
-    )
-
-    def build_tool(self, autotx: AutoTx) -> Callable:
-        def run(
-            amount: Annotated[float, "Amount given by the user to transfer. The function will take care of converting the amount to needed decimals."],
-            receiver: Annotated[str, "The receiver's address or ENS domain"]
-        ) -> str:
-            web3 = load_w3()
-      
-            receiver_addr = ETHAddress(receiver, web3)
-        
-            tx = build_transfer_eth(web3, ETHAddress(ADDRESS_ZERO, web3), receiver_addr, amount)
-        
-            prepared_tx = PreparedTx(f"Transfer {amount} ETH to {str(receiver_addr)}", tx)
-
-            autotx.transactions.append(prepared_tx)
-
-            print(f"Prepared transaction: {prepared_tx.summary}")
-            
-            return prepared_tx.summary
-
-
-        return run
-        
-class TransferERC20Tool(AutoTxTool):
-    name: str = "prepare_transfer_erc20_transaction"
-    description: str = dedent(
-        """
-        Prepares a transfer transaction for given amount in decimals for given token.
+        Prepares a transfer transaction for given amount in decimals for a token
         """
     )
 
@@ -68,52 +40,34 @@ class TransferERC20Tool(AutoTxTool):
             receiver: Annotated[str, "The receiver's address or ENS domain"],
             token: Annotated[str, "Symbol of token to transfer"]
         ) -> str:
-            token_address = autotx.network.tokens[token.lower()]
             web3 = load_w3()
-      
             receiver_addr = ETHAddress(receiver, web3)
-        
-            tx = build_transfer_erc20(web3, token_address, receiver_addr, amount)
-        
-            prepared_tx = PreparedTx(f"Transfer {amount} {token} to {str(receiver_addr)}", tx)
-        
+            token_address = ETHAddress(autotx.network.tokens[token.lower()], web3)
+
+            prepared_tx: PreparedTx | None = None
+
+            if token_address.hex == NATIVE_TOKEN_ADDRESS:
+                tx = build_transfer_eth(web3, ETHAddress(ADDRESS_ZERO, web3), receiver_addr, amount)
+
+                prepared_tx = PreparedTx(f"Transfer {amount} ETH to {str(receiver_addr)}", tx)
+            else:
+                tx = build_transfer_erc20(web3, token_address, receiver_addr, amount)
+            
+                prepared_tx = PreparedTx(f"Transfer {amount} {token} to {str(receiver_addr)}", tx)
+
             autotx.transactions.append(prepared_tx)
-        
+            
             print(f"Prepared transaction: {prepared_tx.summary}")
-           
+            
             return prepared_tx.summary
 
         return run
-    
-class GetETHBalanceTool(AutoTxTool):
-    name: str = "get_eth_balance"
+
+class GetTokenBalanceTool(AutoTxTool):
+    name: str = "get_token_balance"
     description: str = dedent(
         """
-        Check owner balance in ETH
-        """
-    )
-
-    def build_tool(self, autotx: AutoTx) -> Callable:
-        def run(
-            owner: Annotated[str, "The owner's address or ENS domain"]
-        ) -> float:
-            web3 = load_w3()
-            
-            owner_addr = ETHAddress(owner, web3)
-
-            eth_balance = web3.eth.get_balance(owner_addr.hex)
-
-            print(f"Fetching ETH balance for {str(owner_addr)}: {eth_balance / 10 ** 18} ETH")
-          
-            return eth_balance / 10 ** 18
-
-        return run
-    
-class GetERC20BalanceTool(AutoTxTool):
-    name: str = "get_erc20_balance"
-    description: str = dedent(
-        """
-        Check owner balance in ERC20 token
+        Check owner balance of token
         """
     )
 
@@ -123,23 +77,26 @@ class GetERC20BalanceTool(AutoTxTool):
             owner: Annotated[str, "The token owner's address or ENS domain"]
         ) -> float:
             web3 = load_w3()
-            token_address = ETHAddress(autotx.network.tokens[token.lower()], web3)
             owner_addr = ETHAddress(owner, web3)
+            token_address = ETHAddress(autotx.network.tokens[token.lower()], web3)
+            
+            balance: float = 0
 
-            balance = get_erc20_balance(web3, token_address, owner_addr)
+            if token_address.hex == NATIVE_TOKEN_ADDRESS:
+                balance = get_eth_balance(web3, owner_addr)
+            else:
+                balance = get_erc20_balance(web3, token_address, owner_addr)
 
             print(f"Fetching {token} balance for {str(owner_addr)}: {balance} {token}")
             
             return balance
 
         return run
-
+    
 class SendTokensAgent(AutoTxAgent):
     name = name
     system_message = system_message
     tools = [
-        TransferETHTool(),
-        TransferERC20Tool(),
-        GetETHBalanceTool(),
-        GetERC20BalanceTool()
+        TransferTokenTool(),
+        GetTokenBalanceTool(),
     ]
