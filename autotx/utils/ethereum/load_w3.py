2603138f6d7ac7f84d56ad830fe136e0344fed39
nerfZael <bogunovij@gmail.com>
2024-04-11 20:05:51 +0200

    exit if no chain rpc url in live env

--- autotx/utils/ethereum/load_w3.py
+++ autotx/utils/ethereum/load_w3.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from web3 import Web3
 from autotx.utils.ethereum.constants import DEVNET_RPC_URL
 from autotx.utils.is_dev_env import is_dev_env
@@ -6,6 +7,9 @@ from autotx.utils.is_dev_env import is_dev_env
 def load_w3() -> Web3:
     rpc_url = DEVNET_RPC_URL if is_dev_env() else os.getenv("CHAIN_RPC_URL")
     
+    if not rpc_url:
+        sys.exit("CHAIN_RPC_URL is not set")
+
     web3 = Web3(Web3.HTTPProvider(rpc_url))
     
     return web3
