98ff69bb3fb4aa477af1059f78a0ec2443b90f6e
nerfZael <bogunovij@gmail.com>
2024-04-11 13:33:43 +0200

    fixed mypy issue

--- autotx/utils/configuration.py
+++ autotx/utils/configuration.py
@@ -18,6 +18,9 @@ smart_account_addr = get_env_vars()
 def get_configuration() -> tuple[ETHAddress | None, LocalAccount, EthereumClient]:
     rpc_url = DEVNET_RPC_URL if is_dev_env() else os.getenv("CHAIN_RPC_URL")
     
+    if not rpc_url:
+        sys.exit("CHAIN_RPC_URL is not set")
+
     web3 = Web3(Web3.HTTPProvider(rpc_url))
 
     for i in range(16):
