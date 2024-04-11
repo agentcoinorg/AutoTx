80f9a13be494fd268c92df2d2b2ece1f64210e4a
nerfZael <bogunovij@gmail.com>
2024-04-11 20:05:38 +0200

    multiple l2 master copy addresses now supported

--- autotx/utils/ethereum/is_valid_safe.py
+++ autotx/utils/ethereum/is_valid_safe.py
@@ -4,7 +4,7 @@ from web3 import Web3
 from gnosis.safe import Safe
 
 from autotx.utils.ethereum.eth_address import ETHAddress
-from .constants import MASTER_COPY_ADDRESS, MASTER_COPY_L2_ADDRESS
+from .constants import MASTER_COPY_ADDRESS, MASTER_COPY_L2_ADDRESSES
 
 def is_valid_safe(client: EthereumClient, safe_address: ETHAddress) -> bool:
     w3 = client.w3
@@ -12,6 +12,6 @@ def is_valid_safe(client: EthereumClient, safe_address: ETHAddress) -> bool:
     if w3.eth.get_code(Web3.to_checksum_address(safe_address.hex)) != w3.to_bytes(hexstr=HexStr("0x")):
         safe = Safe(Web3.to_checksum_address(safe_address.hex), client)
         master_copy_address = safe.retrieve_master_copy_address()
-        return master_copy_address in [MASTER_COPY_ADDRESS, MASTER_COPY_L2_ADDRESS]
+        return master_copy_address in [MASTER_COPY_ADDRESS] or master_copy_address in MASTER_COPY_L2_ADDRESSES
     else:
         return False
