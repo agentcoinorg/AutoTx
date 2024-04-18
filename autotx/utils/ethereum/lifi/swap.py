7ea06123b82dbe7311dc4bbd66f76a084b18499a
Cesar <cesarbrazon10@gmail.com>
2024-04-19 00:51:48 +0200

    chore: add 1% to amount when output amount is set

--- autotx/utils/ethereum/lifi/swap.py
+++ autotx/utils/ethereum/lifi/swap.py
@@ -32,8 +32,8 @@ def build_swap_transaction(
         token_out_price_in_usd = Lifi.get_token_price(token_out_address, chain)
         amount_token_to_buy = (token_out_price_in_usd * amount) / token_in_price_in_usd
         amount_in_integer = int(amount_token_to_buy * (10**decimals))
-        # add slippage plus 0.05% to ensure we get the expected amount
-        amount_in_integer = int(amount_in_integer * 0.005 + amount_in_integer)
+        # add 0.01% to ensure we get the expected amount
+        amount_in_integer = int(amount_in_integer * 0.01 + amount_in_integer)
 
     quote = Lifi.get_quote(
         token_in_address, token_out_address, amount_in_integer, _from, chain, SLIPPAGE
