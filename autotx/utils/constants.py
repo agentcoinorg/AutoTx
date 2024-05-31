e8e61b4a3c54f3775c0827cc20774e4e50f56a7f
nerfZael <bogunovij@gmail.com>
2024-05-31 16:37:57 +0200

    default model is now gpt 4o

--- autotx/utils/constants.py
+++ autotx/utils/constants.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-OPENAI_MODEL_NAME = os.environ.get("OPENAI_MODEL_NAME", "gpt-4-turbo")
+OPENAI_MODEL_NAME = os.environ.get("OPENAI_MODEL_NAME", "gpt-4o")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", None)
 OPENAI_BASE_URL = os.environ.get("OPENAI_BASE_URL", "https://api.openai.com/v1")
 COINGECKO_API_KEY = os.environ.get("COINGECKO_API_KEY", None)
